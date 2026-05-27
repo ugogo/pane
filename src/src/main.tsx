4db8fffa9e8c40d6e35a48967d1872676a8719c9
Ugo Onali <ugo.j.onali@gmail.com>
2026-05-27 13:14:57 +0200

    fix: prevent blank launch window

--- src/src/main.tsx
+++ src/src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 import { App } from "./App";
 import "./styles.css";
 
@@ -8,3 +9,11 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <App />
   </React.StrictMode>,
 );
+
+if (!new URL(window.location.href).searchParams.has("view")) {
+  requestAnimationFrame(() => {
+    requestAnimationFrame(() => {
+      void getCurrentWindow().show().catch(console.error);
+    });
+  });
+}
